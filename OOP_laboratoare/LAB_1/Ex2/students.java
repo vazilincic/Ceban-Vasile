efaee38e050aa5c2b1151a49be9ec9463a066c45
vazilincic <84223748+vazilincic@users.noreply.github.com>
2021-10-19 08:02:08 +0300

    Create students.java

---
+++ OOP_laboratoare/LAB_1/Ex2/students.java
@@ -0,0 +1,13 @@
+package com.company;
+public class students
+{
+    public students(String name, int age, float mark)
+    {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+    }
+    String name;
+    int age;
+    float mark;
+}
